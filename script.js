9da697c43ef4784e0e4ba09505f1f28a18a64332
tinoradice <valentinoradice2007@gmail.com>
2026-04-17 04:12:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-script.js
+const items = document.querySelectorAll(".fade-up");
+
+function showOnScroll() {
+  const trigger = window.innerHeight * 0.9;
+
+  items.forEach((item) => {
+    const top = item.getBoundingClientRect().top;
+    if (top < trigger) {
+      item.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", showOnScroll);
+window.addEventListener("load", showOnScroll);
